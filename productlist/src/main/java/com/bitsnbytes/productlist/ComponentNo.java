156c7f524402ea373ebdb748eca4c4c4a4f2e33c
dheerajmittal03 <60285329+dheerajmittal03@users.noreply.github.com>
2025-11-27 00:25:44 +0530

    ok
    ok

---
+++ productlist/src/main/java/com/bitsnbytes/productlist/ComponentNo.java
@@ -0,0 +1,10 @@
+package com.bitsnbytes.productlist;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentNo {
+public ComponentNo() {
+	System.out.println("HJJELELE ");
+}
+}
